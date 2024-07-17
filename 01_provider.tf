df4133a407bcde92ee5323bcb12500cef1d8846c
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-07-17 15:58:36 -0400

    add argo for test

--- 01_provider.tf
+++ 01_provider.tf
@@ -6,6 +6,18 @@ provider "kubernetes" {
   host = module.eks.cluster_endpoint
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
+
 terraform {
   required_version = "~> 1.7"
   required_providers {
@@ -16,6 +28,10 @@ terraform {
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.31.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "~> 2.14.0"
     }
   }
 }
