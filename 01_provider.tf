5c18ce1ff2213ebe4a3813be184770ef92a0445d
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-07-18 16:15:49 -0400

    update to latest clean

--- 01_provider.tf
+++ 01_provider.tf
@@ -9,10 +9,10 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--output", "json"]
       command     = "aws"
     }
   }
