815951fa824be08394aed4d51534c679d793e1ec
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-07-23 13:12:24 -0400

    add variables

--- 03_eks.tf
+++ 03_eks.tf
@@ -10,7 +10,7 @@ module "eks" {
 
   # Give Terraform admin access to the cluster
   enable_cluster_creator_admin_permissions = true
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access           = true
 
   eks_managed_node_groups = {
     node_group = {
--- 09_helm_release.tf
+++ 09_helm_release.tf
@@ -1,27 +1,27 @@
 resource "helm_release" "argocd" {
-  name = "argocd"
+  name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
-  chart = "argo-cd"
-  version = "4.5.2"
+  chart      = "argo-cd"
+  version    = "7.3.10"
 
   namespace = "argocd"
 
   create_namespace = true
 
   set {
-    name = "server.service.type"
+    name  = "server.service.type"
     value = "LoadBalancer"
   }
 
   set {
-    name = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+    name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
     value = "nlb"
   }
 }
 
 data "kubernetes_service" "argocd_server" {
   metadata {
-    name = "argocd-server"
+    name      = "argocd-server"
     namespace = helm_release.argocd.namespace
   }
 }
