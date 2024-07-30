159e5dcf237da3acddb443601e5b7defdd910931
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-07-30 16:48:13 -0400

    progress on tofugrunt rewrite

--- 07_secrets.tf
+++ 07_secrets.tf
@@ -1,5 +1,19 @@
+data "aws_iam_policy_document" "workload_identity_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+    ]
+    resources = [ 
+      "arn:aws:secretmanager:${var.primary_region}:${data.aws_caller_identity.current.account_id}:secret:*",
+    ]
+  }
+}
+
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "rds-mysql-entry-${local.cleanstamp}"
+  name = "${var.application_name}-${var.environment_name}-connection-string"
+  description = "Database connection string"
 }
 
 resource "aws_secretsmanager_secret_version" "db_credentials_version" {
@@ -8,5 +22,24 @@ resource "aws_secretsmanager_secret_version" "db_credentials_version" {
     username = var.db_username
     password = var.db_password
   })
+}
+
+resource "helm_release" "csi_secrets_store" {
+  name = "csi-secrets-store"
+  repository = "https://kubernetes-sigs.github.io/secrets-stroe-csi-driver/charts"
+  chart = "secrets-store-csi-driver"
+  namespace = "kube-system"
+
+  set {
+    name = "syncSecret.enabled"
+    value = "true"
+  }
+}
+
+resource "helm_release" "aws_secrets_proviver" {
+  name = "secrets-provider-aws"
+  repository = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
+  chart = "secrets-store-csi-driver-provider-aws"
+  namespace = "kube-system"
 }
 
