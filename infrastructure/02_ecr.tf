ec3d406d169ce93d2f11f77009ad516a50a63e6e
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-08-02 15:37:03 -0400

    complete rough draft of tofunetes

--- infrastructure/02_ecr.tf
+++ infrastructure/02_ecr.tf
@@ -1,18 +1,29 @@
 locals {
   repository_list = ["announcements"]
-  repositories = { for name in local.repository_list : name => name }
+  repositories    = { for name in local.repository_list : name => name }
 }
 
 resource "aws_ecr_repository" "main" {
   for_each = local.repositories
-  name = "ecr-${var.application_name}-${var.environment_name}-${each.key}"
+
+  name                 = "ecr-${var.application_name}-${var.environment_name}-${each.key}"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  tags = {
+    application = var.application_name
+    environment = var.envrionment_name
+  }
+}
+
+resource "aws_iam_group" "ecr_image_pushers" {
+  name = "${var.application_name}-${var.environment_name}-ecr-image-pushers"
 }
 
 resource "aws_iam_group_policy" "ecr_image_pushers" {
   for_each = local.repositories
-  name = "${var.application_name}-${var.environment_name}-${each.key}-ecr-image-push-policy"
-  group = aws_iam_group.ecr_image_pushers.name
+  name     = "${var.application_name}-${var.environment_name}-${each.key}-ecr-image-push-policy"
+  group    = aws_iam_group.ecr_image_pushers.name
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -34,7 +45,7 @@ resource "aws_iam_group_policy" "ecr_image_pushers" {
 }
 
 resource "aws_iam_group_membership" "ecr_image_pushers" {
-  name = "${var.application_name}-${var.environment_name}-ecr-image-push-membership"
+  name  = "${var.application_name}-${var.environment_name}-ecr-image-push-membership"
   users = var.ecr_image_pushers
   group = aws_iam_group.ecr_image_pushers.name
 }
