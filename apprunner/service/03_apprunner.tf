570f79d726ff6ba4382a580a3e2ba665a6bb02b1
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-08-20 16:17:53 -0400

    Fix reorginzation bugs

--- apprunner/service/03_apprunner.tf
+++ apprunner/service/03_apprunner.tf
@@ -13,7 +13,7 @@ resource "aws_apprunner_service" "app_services" {
     }
 
     authentication_configuration {
-      access_role_arn = aws_iam_role.apprunner_role.arn
+      access_role_arn = var.access_role_arn
     }
   }
 
