1fad08df1584e434740e70e7874444a6ee34472c
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-10-15 10:53:31 -0400

    Test commit

--- test/appenvlist_test.go
+++ test/appenvlist_test.go
@@ -1,78 +1,78 @@
 package test
 
 import (
-	"testing"
+  "testing"
 
-	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
+  "github.com/gruntwork-io/terratest/modules/terraform"
+  "github.com/stretchr/testify/assert"
 )
 
 func TestAppEnvListFlattening(t *testing.T) {
-  
-	terraformOptions := &terraform.Options{
-		TerraformDir: "../modules/appenvlist",
 
-		Vars: map[string]interface{}{
-			"applications": []string{
-				"announcements", "template",
-			},
-			"environments": []string{
-				"dev", "test",
-			},
-		},
-	}
+  terraformOptions := &terraform.Options{
+    TerraformDir: "../modules/appenvlist",
 
-	defer terraform.Destroy(t, terraformOptions)
-	terraform.InitAndApply(t, terraformOptions)
+    Vars: map[string]interface{}{
+      "applications": []string{
+        "announcements", "template",
+      },
+      "environments": []string{
+        "dev", "test",
+      },
+    },
+  }
 
-	actualAppEnvList := terraform.OutputList(t, terraformOptions, "app_env_list")
+  defer terraform.Destroy(t, terraformOptions)
+  terraform.InitAndApply(t, terraformOptions)
 
-	expectedAppEnvList := []string{
-		"map[app:announcements env:dev]",
-		"map[app:announcements env:test]",
-		"map[app:template env:dev]",
-		"map[app:template env:test]",
-	}
+  actualAppEnvList := terraform.OutputList(t, terraformOptions, "app_env_list")
 
-	assert.Equal(t, expectedAppEnvList, actualAppEnvList, "The flattened list is incorrect.")
+  expectedAppEnvList := []string{
+    "map[app:announcements env:dev]",
+    "map[app:announcements env:test]",
+    "map[app:template env:dev]",
+    "map[app:template env:test]",
+  }
+
+  assert.Equal(t, expectedAppEnvList, actualAppEnvList, "The flattened list is incorrect.")
 
 }
 
 func TestLargerAppEnvListFlattening(t *testing.T) {
 
-	terraformOptions := &terraform.Options{
-		TerraformDir: "../modules/appenvlist",
+  terraformOptions := &terraform.Options{
+    TerraformDir: "../modules/appenvlist",
 
-		Vars: map[string]interface{}{
-			"applications": []string{
-				"announcements", "template", "events", "herald",
-			},
-			"environments": []string{
-				"dev", "test", "prod",
-			},
-		},
-	}
-	
-	defer terraform.Destroy(t, terraformOptions)
-	terraform.InitAndApply(t, terraformOptions)
+    Vars: map[string]interface{}{
+      "applications": []string{
+        "announcements", "template", "events", "herald",
+      },
+      "environments": []string{
+        "dev", "test", "prod",
+      },
+    },
+  }
 
-	actualAppEnvList := terraform.OutputList(t, terraformOptions, "app_env_list")
+  defer terraform.Destroy(t, terraformOptions)
+  terraform.InitAndApply(t, terraformOptions)
 
-	expectedAppEnvList := []string{
-		"map[app:announcements env:dev]",
-		"map[app:announcements env:test]",
-		"map[app:announcements env:prod]",
-		"map[app:template env:dev]",
-		"map[app:template env:test]",
-		"map[app:template env:prod]",
-		"map[app:events env:dev]",
+  actualAppEnvList := terraform.OutputList(t, terraformOptions, "app_env_list")
+
+  expectedAppEnvList := []string{
+    "map[app:announcements env:dev]",
+    "map[app:announcements env:test]",
+    "map[app:announcements env:prod]",
+    "map[app:template env:dev]",
+    "map[app:template env:test]",
+    "map[app:template env:prod]",
+    "map[app:events env:dev]",
                 "map[app:events env:test]",
                 "map[app:events env:prod]",
                 "map[app:herald env:dev]",
                 "map[app:herald env:test]",
-                "map[app:herald env:prod]", 
-	}
+                "map[app:herald env:prod]",
+  }
 
-	assert.Equal(t, expectedAppEnvList, actualAppEnvList, "The flattened list is incorrect.")
+  assert.Equal(t, expectedAppEnvList, actualAppEnvList, "The flattened list is incorrect.")
 
 }
