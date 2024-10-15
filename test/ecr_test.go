07da7f9b8ac40c69796dc060564608e9204e25ec
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-10-15 10:58:02 -0400

    fix issues; add pre-commit hook

--- test/ecr_test.go
+++ test/ecr_test.go
@@ -1,52 +1,52 @@
 package test
 
 import (
-  "testing"
-  "path/filepath"
+	"testing"
+	"path/filepath"
 
-  "github.com/gruntwork-io/terratest/modules/terraform"
-  test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
-  "github.com/stretchr/testify/assert"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestECRCreation(t *testing.T) {
-  t.Parallel()
+	t.Parallel()
 
-  tempTestFolder := filepath.Join(".", "/stages")
+	tempTestFolder := filepath.Join(".", "/stages")
 
-  defer test_structure.RunTestStage(t, "teardown_ecr_module", func() {
+	defer test_structure.RunTestStage(t, "teardown_ecr_module", func() {
 
-    ecrOptions := test_structure.LoadTerraformOptions(t, tempTestFolder)
-    terraform.Destroy(t, ecrOptions)
-  })
+		ecrOptions := test_structure.LoadTerraformOptions(t, tempTestFolder)
+		terraform.Destroy(t, ecrOptions)
+	})
 
-  test_structure.RunTestStage(t, "deploy_ecr_module", func() {
+	test_structure.RunTestStage(t, "deploy_ecr_module", func() {
 
-    ecrOptions := &terraform.Options{
-      TerraformDir: "../modules/ecr",
-      Vars: map[string]interface{}{
-        "applications": []string{
-                                        "announcements", "template",
-                                },
-                                "environments": []string{
-                                        "dev", "test",
-                                },
-      },
-    }
+		ecrOptions := &terraform.Options{
+			TerraformDir: "../modules/ecr",
+			Vars: map[string]interface{}{
+				"applications": []string{
+																				"announcements", "template",
+																},
+																"environments": []string{
+																				"dev", "test",
+																},
+			},
+		}
 
-  test_structure.SaveTerraformOptions(t, tempTestFolder, ecrOptions)
+	test_structure.SaveTerraformOptions(t, tempTestFolder, ecrOptions)
 
-  terraform.InitAndApply(t, ecrOptions)
+	terraform.InitAndApply(t, ecrOptions)
 
-  actualRepoNames := terraform.OutputList(t, ecrOptions, "ecr_repo_names")
+	actualRepoNames := terraform.OutputList(t, ecrOptions, "ecr_repo_names")
 
-  expectedRepoNames := []string{
-    "announcements-dev",
-    "announcements-test",
-    "template-dev",
-    "template-test",
-  }
+	expectedRepoNames := []string{
+		"announcements-dev",
+		"announcements-test",
+		"template-dev",
+		"template-test",
+	}
 
-  assert.Equal(t, expectedRepoNames, actualRepoNames, "The ECR repository names are incorrect.")
-  })
+	assert.Equal(t, expectedRepoNames, actualRepoNames, "The ECR repository names are incorrect.")
+	})
 }
