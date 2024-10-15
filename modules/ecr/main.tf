8af6bca00788084600f66db735318d1c7b0dba75
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-10-15 11:16:41 -0400

    ignore tf files for editorconfig

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -15,8 +15,8 @@ module "appenvlist" {
 }
 
 resource "aws_ecr_repository" "app_repos" {
-  for_each = {for combo in module.appenvlist.app_env_list : "${combo.app}-${combo.env}" => combo }
+  for_each = { for combo in module.appenvlist.app_env_list : "${combo.app}-${combo.env}" => combo }
 
-  name = "${each.value.app}-${each.value.env}"
+  name         = "${each.value.app}-${each.value.env}"
   force_delete = true
 }
--- modules/ecr/variables.tf
+++ modules/ecr/variables.tf
@@ -1,9 +1,9 @@
 variable "applications" {
-  type = list(string)
+  type        = list(string)
   description = "List of application names"
 }
 
 variable "environments" {
-  type = list(string)
+  type        = list(string)
   description = "List of application environments"
 }
