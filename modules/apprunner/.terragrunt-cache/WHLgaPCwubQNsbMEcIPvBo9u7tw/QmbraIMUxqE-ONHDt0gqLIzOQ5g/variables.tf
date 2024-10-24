53eb98c7db1c0af92927b9d2053939bdfb14c2c5
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-10-24 15:35:57 -0400

    add remote state; cleanup terragrunt caches

--- modules/apprunner/.terragrunt-cache/WHLgaPCwubQNsbMEcIPvBo9u7tw/QmbraIMUxqE-ONHDt0gqLIzOQ5g/variables.tf
+++ modules/apprunner/.terragrunt-cache/WHLgaPCwubQNsbMEcIPvBo9u7tw/QmbraIMUxqE-ONHDt0gqLIzOQ5g/variables.tf
@@ -1,28 +1,27 @@
 variable "applications" {
   type        = list(string)
   description = "A list of available applications"
-  default = ["announcements"]
+  default     = ["announcements"]
 }
 
 variable "environments" {
   type        = list(string)
   description = "A list of available environments"
-  default = ["dev"]
-}
-
-variable "is_tofu_test_environment" {
-  type        = bool
-  description = "Determines if Tofu Test environment is active"
-  default     = false
+  default     = ["dev"]
 }
 
 variable "app_env_list" {
   type = list(object({
-          app = string
-          env = string
-         }))
+    app = string
+    env = string
+  }))
 }
 
 variable "ecr_repo_names" {
   type = list(string)
+}
+
+variable "region" {
+  type = string
+  default = "us-east-1"
 }
