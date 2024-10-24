55a5e7c45731147cffa34218236ca102102485f5
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-10-24 13:31:21 -0400

    fix tests to match terragrunt

--- modules/ecr/variables.tf
+++ modules/ecr/variables.tf
@@ -4,16 +4,6 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "applications" {
-  type        = list(string)
-  description = "List of application names"
-}
-
-variable "environments" {
-  type        = list(string)
-  description = "List of application environments"
-}
-
 variable "app_env_list" {
   type = list(object({
            app = string
