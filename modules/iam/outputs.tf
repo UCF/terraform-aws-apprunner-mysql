c682e85d7a821fb7973bfac826dc3cd43cc3a793
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-10-30 11:30:01 -0400

    add Github IAM

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -2,3 +2,13 @@ output "apprunner_access_role_arn" {
   value       = aws_iam_role.apprunner_role.arn
   description = "The ARN of the AppRunner access IAM role"
 }
+
+output "github_access_role_arn" {
+  value       = aws_iam_role.ecraccess_role.arn
+  description = "The ARN of the ECR access IAM role"
+}
+
+output "oidc_arn" {
+  value = module.github-oidc.oidc_provider_arn
+  description = "Github OIDC ARN"
+}
