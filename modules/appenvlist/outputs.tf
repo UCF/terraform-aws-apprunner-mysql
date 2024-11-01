0bb2b1f712669ddc7558eea2aa357778cfbf33fb
ucfchandra <chandra.kethireddy@ucf.edu>
2024-11-01 15:32:59 -0400

    clarify output

--- modules/appenvlist/outputs.tf
+++ modules/appenvlist/outputs.tf
@@ -1,4 +1,4 @@
 output "app_env_list" {
-  description = "A combined list of required applications and their environments"
+  description = "A combined list of objects naming required applications and their environments"
   value       = local.app_env_list
 }
