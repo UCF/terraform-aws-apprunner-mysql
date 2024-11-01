fc395491305131d1422b2f790d5c12005b2e3347
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-11-01 15:02:54 -0400

    make outputs comply with HCL style guide

--- modules/appenvlist/outputs.tf
+++ modules/appenvlist/outputs.tf
@@ -1,4 +1,10 @@
 output "app_env_list" {
+
+  type = list(object({
+    app = string
+    env = string
+  }))
+
+  description = "A combined list of required applications and their environments"
   value       = local.app_env_list
-  description = "A combination of applications and their required environments"
 }
