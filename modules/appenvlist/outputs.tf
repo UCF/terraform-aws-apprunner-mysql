4f6ad82b0b433fee5281be57b3c88d5dfa07d95a
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-11-01 15:09:24 -0400

    remove mistake with type parameter added to app_env_list output

--- modules/appenvlist/outputs.tf
+++ modules/appenvlist/outputs.tf
@@ -1,10 +1,4 @@
 output "app_env_list" {
-
-  type = list(object({
-    app = string
-    env = string
-  }))
-
   description = "A combined list of required applications and their environments"
   value       = local.app_env_list
 }
