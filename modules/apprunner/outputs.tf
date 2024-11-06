3323763137acdca80b037d328485c78016e37c37
ucfchandra <chandra.kethireddy@ucf.edu>
2024-11-06 12:03:42 -0500

    clean up mod 5

--- modules/apprunner/outputs.tf
+++ modules/apprunner/outputs.tf
@@ -1,3 +1,7 @@
 output "DNS_records" {
   value = [for domain in aws_apprunner_custom_domain_association.domains : domain.certificate_validation_records]
 }
+
+output "default_domain" {
+  value = [for service in aws_apprunner_service.app_services : service.service_url]
+}
