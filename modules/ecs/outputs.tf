dd10792ea53529deafc3acbe3280cda37cb62672
Chandra <chandra.kethireddy@ucf.edu>
2024-12-26 08:50:20 -0500

    complete module up to IAM; passing tests

--- modules/ecs/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vitess_endpoint" {
-  value = "${data.aws_network_interface.task_eni.private_ip}:15999"
-  description = "The endpoint for the Vitess vtgate service."
-}
--- modules/ecs/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.region
-}
-
-provider "mysql" {
-  endpoint = "${data.aws_network_interface.task_eni.private_ip}:15999" 
-  username = "admin"
-  password = var.adminpw
-}
